80e71bd8f985fddde56ed837253a894a88152e72
Kostia Kotliarov <zemixx@bigmir.net>
2017-08-30 14:14:32 +0300

    Struct example

---
+++ struct.c
@@ -0,0 +1,69 @@
+
+#include <stdio.h>
+
+struct structOne
+{
+	int a;
+	int b;
+	int c;
+};
+
+struct structTwo
+{
+	long a;
+	int b;
+	int c;
+};
+
+struct structThree
+{
+	char a;
+	char b;
+	char c;
+};
+
+struct structFour
+{
+	char a;
+	int  b;
+	char c;
+};
+
+struct structFive
+{
+	char a;
+	int  b;
+	char c;
+};
+
+int main(void)
+{
+
+
+	struct structOne one;
+	struct structTwo two;
+	struct structThree three;
+	struct structFour four;
+	struct structFive five = {'a', 3};
+	
+	struct structFive *ptrFive;
+	
+	ptrFive = &five;
+	
+	
+	
+	printf("%lu\n", sizeof(one));
+	printf("%lu\n", sizeof(two));
+	printf("%lu\n", sizeof(three));
+	printf("%lu\n", sizeof(four));
+	printf("%lu\n", sizeof(five));
+	
+	printf("%c %d %c\n", five.a, five.b, five.c);
+	printf("%c %d %c\n", (*ptrFive).a, (*ptrFive).b, (*ptrFive).c);
+	printf("%c %d %c\n", ptrFive->a, ptrFive->b, ptrFive->c);
+	
+	
+
+	return 0;
+}
+
