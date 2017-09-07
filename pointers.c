e65140ab42526ad2d54e569ef06f7ac6e636f300
Kostiantyn Kotliarov <zemixx@bigmir.net>
2017-09-07 16:25:00 +0300

    pointers learn

--- pointers.c
+++ pointers.c
@@ -70,9 +70,53 @@ int main(void)
 	int d = 33;
 	int * const dPtr = &d;
 	
-	dPtr = &d; //wrong
+//	dPtr = &d; //wrong
 	*dPtr = 7; // ok
+	
+	//pointer
+	
+	int *arr[8];      // array of 8 pointers to int elements
+	
+	int *(arr1[8]);   // array of 8 pointers to int elements
+	
+	int (*arr2)[8];   // pointer to array of 8 int elements
+	
+	int a0 = 0;
+	int a1 = 10;
+	int a2 = 20;
+	int a3 = 30;
+	int a4 = 40;
+	int a5 = 50;
+	int a6 = 60;
+	int a7 = 70;
+	
+	arr[0] = &a0;
+	arr[1] = &a1;
+	arr[2] = &a2;
+	arr[3] = &a3;
+	arr[4] = &a4;
+	arr[5] = &a5;
+	arr[6] = &a6;
+	arr[7] = &a7;
+	
+	arr1[0] = &a0;
+	arr1[1] = &a1;
+	arr1[2] = &a2;
+	arr1[3] = &a3;
+	arr1[4] = &a4;
+	arr1[5] = &a5;
+	arr1[6] = &a6;
+	arr1[7] = &a7;
 
+	int mass[8] = {7,6,5,4,3,2,1,0};
+	
+	arr2 = &mass;
+	printf("%d %d %d %d %d %d %d %d\n", a0, a1, a2, a3, a4, a5, a6, a7);
+	printf("%d %d %d %d %d %d %d %d\n", *arr[0], *arr[1], *arr[2], *arr[3], *arr[4], *arr[5], *arr[6], *arr[7]);
+	printf("%d %d %d %d %d %d %d %d\n", *arr1[0], *arr1[1], *arr1[2], *arr1[3], *arr1[4], *arr1[5], *arr1[6], *arr1[7]);
+	
+	printf("%d %d %d %d %d %d %d %d\n", *arr2[0], *arr2[1], *arr2[2], *arr2[3], *arr2[4], *arr2[5], *arr2[6], *arr2[7]);
+	printf("%d %d %d %d %d %d %d %d\n", *arr2[0], *(arr2[0] + 1), *(arr2[0] + 2), *(arr2[0] + 3), *(arr2[0] + 4), *(arr2[0] + 5), *(arr2[0] + 6), *(arr2[0] + 7));
 	return 0;
 }
 
